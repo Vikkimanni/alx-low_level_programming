60de2b0f65ec6cf49acc42b1e8c1b7db47a7eb6e
AJAYI VICTOR <Vikkimanni@ajayiv112@gmail.com>
2022-03-11 11:59:39 -0800

    hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  *main - entry point
@@ -7,6 +6,4 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+puts("\"Programming is like building a multilingual puzzle"):
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *My hello, print task
+ *Return: return 0 after program execution
+ */
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
