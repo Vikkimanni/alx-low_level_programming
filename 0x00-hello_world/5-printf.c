0bec0459f7b38b9e1597bf68ab61d7dd15df377b
AJAYI VICTOR <Vikkimanni@ajayiv112@gmail.com>
2022-03-11 12:25:51 -0800

    hjh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  *main - entry point
- *
- *Return: 0 after printing the function 
+ *My hello, printf task
+ *Return: 1 after program execution 
  */
 int main (void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
