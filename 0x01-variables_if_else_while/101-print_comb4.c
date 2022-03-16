b7fdd62d26a6bd2e96aaa69f26daeabad667384e
AJAYI VICTOR <Vikkimanni@ajayiv112@gmail.com>
2022-03-16 14:43:44 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all possible different combo of 3 digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 1000; i++)
+	{
+
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 10; /* unit */
+
+		if (j < k && k < 1)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
