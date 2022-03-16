9006b63c136c08e851c606f4c6ce4fd4f3ee6c6c
AJAYI VICTOR <Vikkimanni@ajayiv112@gmail.com>
2022-03-16 15:43:37 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints first 50 fibonacci numbers, starting with 1 and 2,
+ * main - prints first 50 Fibonacci numbers, starting with 1 and 2,
  * 	  separated by a comma followed by a space.
  *
- * 	  Return: Always 0
+ * 	  Return: Always 0.
  */
 
 int main(void)
@@ -23,7 +23,7 @@ int main(void)
 		if (count == 49)
 			printf("\n");
 		else
-			printf(", ");
+			printf(" , ");
 	}
 
 	return (0);
