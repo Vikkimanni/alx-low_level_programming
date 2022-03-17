73910a83ba1c5c7026aaf185aba901af115538d4
AJAYI VICTOR <Vikkimanni@ajayiv112@gmail.com>
2022-03-17 14:24:31 -0700

    alpa

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - alphabet case checker. Returns 1 if true and 0 if false
+ * @c: integer parameer
+ *
+ * Return: Returns value
+ */
+
+int _isalpha(int c)
+{
+	int value;
+
+	if ((c >= 'A' || c >= 'a') && (c <= 'Z' || c <= 'z'))
+	{
+		value = 1;
+	}
+	else
+	{
+		value = 0;
+	}
+	return (value);
+}
